c535e0470c495b5967c528fb4be53e559a43d737
unknown <patidaranil0791@gmail.com>
2022-02-28 15:51:15 +0530

    Animated Site

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Features from './components/features/Features';
+
+function App() {
+  return (
+   <>
+   <Header />
+    <Home />
+    <About />
+    <Features />
+   </>
+  );
+}
+
+export default App;
